a79d979226895cbb52c7a536fcf88e60b29a267a
M Macleod <malcolm@macleodhome.com>
2017-03-05 21:00:05 +0000

    Add initial persistence

---
+++ hello-persistence/Sources/App/main.swift
@@ -0,0 +1,26 @@
+import Vapor
+import VaporPostgreSQL
+
+
+let drop = Droplet()
+try drop.addProvider(VaporPostgreSQL.Provider.self)
+
+
+drop.get { req in
+    return try drop.view.make("welcome", [
+    	"message": drop.localization[req.lang, "welcome", "title"]
+    ])
+}
+
+drop.get("version") { req in
+    if let db = drop.database?.driver as? PostgreSQLDriver {
+        let version = try db.raw("SELECT version()")
+        return try JSON(node: version)
+    } else {
+        return "No db connection"
+    }
+}
+
+drop.resource("posts", PostController())
+
+drop.run()
